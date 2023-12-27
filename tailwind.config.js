939af171ac7e3185213a8d30cd3aad8ee37aaece
Luciano Adamczuk <lucianoadamczuk@gmail.com>
2023-12-27 12:09:56 -0300

    feat: :heavy_plus_sign: prettier husky lintstaged and color base

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      dark: "#0D0D0D",
+      gray: "#BFBFBF",
+      light: "#F2F2F2",
+      link: "#6179DE",
+    },
+  },
+  plugins: [],
+};
